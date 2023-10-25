2133fa1117e0a1e03a164536f7fc47472573d272
VMCantador <vezio.cantador@etec.sp.gov.br>
2023-10-25 16:49:12 -0300

    Atualização EtecBook Server

--- EtecBook.Server/EtecBookAPI/Data/AppDbContext.cs
+++ EtecBook.Server/EtecBookAPI/Data/AppDbContext.cs
@@ -1,13 +1,13 @@
-
 using EtecBookAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
+
 namespace EtecBookAPI.Data;
 
-public class AppDbContext : DbContext
+ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {}
+    {}     
 
     public DbSet<AppUser> Users { get; set; }
 
--- EtecBook.Server/EtecBookAPI/Models/AppUser.cs
+++ EtecBook.Server/EtecBookAPI/Models/AppUser.cs
@@ -4,34 +4,34 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace EtecBookAPI.Models;
 
 [Table("Usuario")]
-public class AppUser
-{
-    [Key]
-    public Guid Id { get; set; }
 
-    [Required]
-    [StringLength(60)]
-    public string Name { get; set; }
+    public class AppUser
+    {
+        [Key]
+        public Guid Id { get; set; }
 
-    [Required]
-    [StringLength(20, MinimumLength = 3)]
-    public string UserName { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string Name { get; set; }
 
-    [Required]
-    public string Password { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string UserName { get; set; }
 
-    public string Token { get; set; }
+        [Required]
+        public string Password { get; set; }
 
-    [Required]
-    [StringLength(20)]
-    public string Role { get; set; }
+        public string Token { get; set; }
 
-    [Required]
-    [EmailAddress]
-    [StringLength(100)]
-    public string Email { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Role { get; set; }
 
-    [StringLength(300)]
-    public string ProfilePicture { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
 
-}
+        [StringLength(300)]
+        public string ProfilePicture { get; set; }
+    }
