077633a505d7f56cf9a1f63aadb4d5409e6d66bb
Vézio Cantador <vezio.cantador@etec.sp.gov.br>
2023-10-26 20:30:02 -0300

    Correção de rotas

--- EtecBookUI/src/app/app-routing.module.ts
+++ EtecBookUI/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMach: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
 ];
